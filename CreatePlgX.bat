823abd849808a0472803966b03669847e6925d5c
alexvallat <alexvallat@f67ab7d1-f845-43b9-8476-148cb4c6c52e>
2018-07-29 14:19:48 +0000

    Clarify build process (Contributed patch by api-dan-air)
    git-svn-id: svn+ssh://svn.code.sf.net/p/kpenhentryview/code/trunk/KPEnhancedEntryView@66 f67ab7d1-f845-43b9-8476-148cb4c6c52e

--- CreatePlgX.bat
+++ CreatePlgX.bat
@@ -14,6 +14,7 @@ echo Compiling PlgX
 "KeePass/KeePass.exe" /plgx-create "%~dp0PlgX" --plgx-prereq-os:Windows --plgx-prereq-kp:2.39
 
 echo Releasing PlgX
+mkdir "Releases/Build Outputs"
 move /y PlgX.plgx "Releases\Build Outputs\KPEnhancedEntryView.plgx"
 
 echo Cleaning up
