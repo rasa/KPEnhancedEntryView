9b2a18e35ff10256af3fdd913c708a7f7576553d
alexvallat <alexvallat@f67ab7d1-f845-43b9-8476-148cb4c6c52e>
2019-03-22 15:01:19 +0000

    Enforce minimum value column width
    git-svn-id: svn+ssh://svn.code.sf.net/p/kpenhentryview/code/trunk/KPEnhancedEntryView@67 f67ab7d1-f845-43b9-8476-148cb4c6c52e

--- KPEnhancedEntryView/FieldsListView.cs
+++ KPEnhancedEntryView/FieldsListView.cs
@@ -63,6 +63,7 @@ namespace KPEnhancedEntryView
 				Hyperlink = true,
 				AspectGetter = rowObject => ((RowObject)rowObject).GetDisplayValue(this),
 				AspectPutter = SetFieldValue,
+				MinimumWidth = 60,
 			};
 			
 			Columns.Add(mFieldNames);
